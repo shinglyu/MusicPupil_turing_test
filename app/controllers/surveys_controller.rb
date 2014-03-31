e1ef574403dd8b8f7f5e68b7df26ccbdee106359
shinglyu <shing.lyu@gmail.com>
2014-03-31 21:14:22 +0800

    Added part 3

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,9 +4,11 @@ class SurveysController < ApplicationController
    def new
       p1TotalQCount= 20
       p2TotalQCount= 20
+      p3TotalQCount= 20
       qCount = 5
       @p1_ids = (1..p1TotalQCount).to_a.sample(qCount) #0~1000 reserved fo part 1
       @p2_ids = (1001..1000+p2TotalQCount).to_a.sample(qCount)
+      @p3_ids = (2001..2000+p3TotalQCount).to_a.sample(qCount)
 
       @survey = Survey.new
       @p1_ids.each do |id|
@@ -14,12 +16,14 @@ class SurveysController < ApplicationController
       end
       @p2_ids.each do |id|
          @survey.p2answers.build(:qid => id)
-      #qCount.times { @survey.p1answers.build(qid)}
+      end
+      @p3_ids.each do |id|
+         @survey.p3answers.build(:qid => id)
       end
    end
 
    def create
-      attrs =  params.require(:survey).permit(:level, :instrument, p1answers_attributes: [:qid, :ans], p2answers_attributes: [:qid, :ans])
+      attrs =  params.require(:survey).permit(:level, :instrument, p1answers_attributes: [:qid, :ans], p2answers_attributes: [:qid, :ans], p3answers_attributes: [:qid, :ans])
       @survey = Survey.new(attrs)
       #@survey.p1answers.build(params[:survey][:p1answers])
       #@survey.p2answers.build(params[:survey][:p2answers])
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,8 @@ class Survey< ActiveRecord::Base
    #has_many :answers, :dependent => :destroy
    has_many :p1answers, :class_name => "Answer", :foreign_key => "survey_id"
    has_many :p2answers, :class_name => "Answer", :foreign_key => "survey_id"
+   has_many :p3answers, :class_name => "Answer", :foreign_key => "survey_id"
    accepts_nested_attributes_for :p1answers
    accepts_nested_attributes_for :p2answers
+   accepts_nested_attributes_for :p3answers
 end
