536bf55390763426543cbebebd14371d754c54f7
shinglyu <shing.lyu@gmail.com>
2014-03-31 17:09:28 +0800

    Randomized option A B

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,33 +1,56 @@
 class SurveysController < ApplicationController
+   before_filter:authenticate, :only => [:index]
+
    def new
       p1TotalQCount= 20
-      #p2TotalQCount= 20
+      p2TotalQCount= 20
       qCount = 5
       @p1_ids = (1..p1TotalQCount).to_a.sample(qCount) #0~1000 reserved fo part 1
-      #@p2_ids = (1001..1000+p2TotalQCount).to_a.sample(qCount)
+      @p2_ids = (1001..1000+p2TotalQCount).to_a.sample(qCount)
 
       @survey = Survey.new
       @p1_ids.each do |id|
-         @survey.answers.build(:qid => id)
+         @survey.p1answers.build(:qid => id)
       end
-      #@p2_ids.each do |id|
-      #   @survey.answers.build(:qid => id)
-      #qCount.times { @survey.answers.build(qid)}
-      #end
+      @p2_ids.each do |id|
+         @survey.p2answers.build(:qid => id)
+      #qCount.times { @survey.p1answers.build(qid)}
+      end
    end
 
    def create
-      attrs =  params.require(:survey).permit(:level, :years, answers_attributes: [:qid, :ans])
+      attrs =  params.require(:survey).permit(:level, :years, p1answers_attributes: [:qid, :ans], p2answers_attributes: [:qid, :ans])
       @survey = Survey.new(attrs)
-      @survey.answers.build(params[:survey][:answers])
+      #@survey.p1answers.build(params[:survey][:p1answers])
+      #@survey.p2answers.build(params[:survey][:p2answers])
       @survey.save
       #render text: params[:survey].inspect
       #answers = params[:survey].select {|key, value| key.to_s.match(/^q\d+/)}
-      redirect_to surveys_path
+      #redirect_to surveys_path
+      redirect_to('/thankyou.html')
    end
 
    def index
       @surveys = Survey.all
+   end
+
+
+   helper_method :logged_in?
+
+   def logged_in?
+        session[:login]
+   end
+   
+   private
+   def authenticate
+     login = authenticate_or_request_with_http_basic do |username, password|
+        username == "admin" && password == "xjp4jp6au4a83"
+     end
+     session[:login] = login
+   end
+   
+   def do_logout
+     session[:login] = nil
    end
 #     private
 #
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,7 @@
 class Survey< ActiveRecord::Base
-   has_many :answers, :dependent => :destroy
-   accepts_nested_attributes_for :answers
+   #has_many :answers, :dependent => :destroy
+   has_many :p1answers, :class_name => "Answer", :foreign_key => "survey_id"
+   has_many :p2answers, :class_name => "Answer", :foreign_key => "survey_id"
+   accepts_nested_attributes_for :p1answers
+   accepts_nested_attributes_for :p2answers
 end
